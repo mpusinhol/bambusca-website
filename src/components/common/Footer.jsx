ca567dca34cf8930b006477bb82bf6247961fe9c
Matheus Pusinhol <mpusinhol@gmail.com>
2018-11-10 18:01:11 -0200

    [10] Style footer @mpusinhol

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,45 +1,27 @@
 import React, {Component} from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink
-} from 'reactstrap';
 
 class Footer extends Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
   render() {
     return (
       <footer id="footer">
-        <Navbar color="dark" dark expand="md">
-          <NavbarBrand href="/">Footer</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <div className="row company-info">
+          <div className="footer-left-data">
+            <a href="/"><span>Sobre nós</span></a>
+            &nbsp;|&nbsp;
+            <a href="/"><span>Política de Privacidade</span></a>
+            &nbsp;|&nbsp;
+            <a href="/"><span>Como trabalhamos</span></a>
+            &nbsp;|&nbsp;
+            <a href="/"><span>Fale Conosco</span></a>
+          </div>
+          <div className="footer-right-data">
+            <span>+55 (16) 0000-0000</span>
+            &nbsp;|&nbsp;
+            <span>Rua Xxxxxx Xxxxxx, 0000</span>
+            &nbsp;|&nbsp;
+            <span>CNPJ 00.000.000/0000-00</span>
+          </div>
+        </div>
       </footer>
     )
   }
