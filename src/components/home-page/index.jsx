908064355afa37fae80c22a67941e2c8a3797ad0
Marcos Nesso <marcosrobertonessojunior@gmail.com>
2018-11-11 10:56:44 -0500

    [9]Work on home's form @marcosrobertonessojunior

--- src/components/home-page/index.jsx
+++ src/components/home-page/index.jsx
@@ -23,6 +23,77 @@ class App extends Component {
             <p className="flexivel">Flexível</p>
           </div>
         </h1>
+        <form className="form-bambusca">
+          <div className="form-group form-field">
+            <div class="form-check form-check-inline check-radio">
+              <input class="form-check-input " type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+              <label class="form-check-label" for="inlineRadio1">Só Ida</label>
+            </div>
+            <div class="form-check form-check-inline check-radio">
+              <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+              <label class="form-check-label" for="inlineRadio2">Ida e Volta</label>
+            </div>
+
+            <div className="align-origin-destiny">
+              <div class="form-group field">
+                <input type="text" class="form-control" id="origin" placeholder="Origem" />
+              </div>
+              <div class="form-group field">
+                <input type="text" class="form-control" id="destiny" placeholder="Destino" />
+              </div>
+            </div>
+
+            <div className="align">
+              <div className="month">
+                <select class="custom-select">
+                  <option selected>Selecione o mês de partida</option>
+                  <option value="1">Janeiro</option>
+                  <option value="2">Fevereiro</option>
+                  <option value="3">Março</option>
+                </select>
+              </div>
+
+              <div className="adults-children-babies">
+                <div class="form-group size">
+                  <input type="number" class="form-control" id="number-of-adults" placeholder="Adultos" />
+                </div>
+                <div class="form-group size">
+                  <input type="number" class="form-control" id="number-of-children" placeholder="Crianças" />
+                </div>
+                <div class="form-group size">
+                  <input type="number" class="form-control" id="number-of-babies" placeholder="Bebês" />
+                </div>
+              </div>
+            </div>
+
+            <p>
+              Por quantos dias deseja viajar?
+            </p>
+
+            <div className="travel-days" >
+
+              <p>
+                De
+            </p>
+              <div class="form-group size">
+                <input type="number" class="form-control" id="initial-day" placeholder="15" />
+              </div>
+              <p>
+                Á
+              </p>
+              <div class="form-group size">
+                <input type="number" class="form-control" id="end-day" placeholder="18" />
+              </div>
+              <p>
+                Dias
+              </p>
+            </div>
+            <div className="button.align">
+              <button type="button">Bambuscar</button>
+            </div>
+
+          </div>
+        </form>
       </div>
     );
   }
